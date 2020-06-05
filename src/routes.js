98ad72cd5a091a8648e32b1e5d523093f343659b
jadiscke <vjtasso@gmail.com>
2020-06-05 11:12:48 -0300

    Change HTML to NJK

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,15 @@ const router = express.Router();
 
 
 router.get('/', (req,res) => {
-   return res.render(`index.html`);
+   return res.render(`index.njk`);
 });
 
 router.get('/create-point', (req,res) => {
-   return res.render(`create-point.html`);
+   return res.render(`create-point.njk`);
 });
 
 router.get('/search', (req,res) => {
-   return res.render(`search.html`);
+   return res.render(`search.njk`);
 });
 
 
