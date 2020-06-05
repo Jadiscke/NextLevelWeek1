b2872afb4dba9bbc663ca6a8176297eda05b0c59
jadiscke <vjtasso@gmail.com>
2020-06-05 09:54:51 -0300

    Change Routes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.sendFile(`${__dirname}/views/index.html`)
-})
+    res.sendFile(`${__dirname}/views/index.html`);
+});
+
+router.get('/create-point', (req,res) => {
+    res.sendFile(`${__dirname}/views/create-point.html`);
+});
+
+router.get('/search', (req,res) => {
+    res.sendFile(`${__dirname}/views/search.html`);
+});
 
 
 module.exports = router;
