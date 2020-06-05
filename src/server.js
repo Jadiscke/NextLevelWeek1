5e4fd916470355b0d2fec04fe8e3973cc33173cc
jadiscke <vjtasso@gmail.com>
2020-06-05 09:27:59 -0300

    Create routes.js And Add Static Files and Routes to Server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const server = express();
+const routes = require(`${__dirname}/routes.js`);
 
 server.use(express.urlencoded({extended:true}));
+server.use(express.static(path.join(__dirname,'..','public')));
 
-
-
+server.use(routes);
 
 const port = process.env.PORT || 3000
 
